66244849162c73ec71afb212c62a87bd7c508572
souzan.mirza@databricks.com <souzan.mirza@databricks.com>
2022-05-05 19:51:35 +0000

    downloads data from source and formats for raw layer

--- iot-wind-turbine/resources/01_download.py
+++ iot-wind-turbine/resources/01_download.py
@@ -4,10 +4,25 @@
 
 # COMMAND ----------
 
+# MAGIC %pip install faker
+
+# COMMAND ----------
+
 try:
-    cloud_storage_path
-except NameError:
     cloud_storage_path = dbutils.widgets.get("cloud_storage_path")
+except NameError:
+    cloud_storage_path = '/'
+
+# COMMAND ----------
+
+# DBTITLE 1,Imports
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+import os
+from scipy.io import loadmat
+import pyspark.pandas as pd
+from faker import Faker
+import datetime
 
 # COMMAND ----------
 
@@ -38,13 +53,8 @@ except NameError:
 
 # COMMAND ----------
 
-ls /tmp/wind_turbine_download
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ##### Unzip wind turbine sensors dataset
-# MAGIC Breakdown of the data downloaded: https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/overview
 
 # COMMAND ----------
 
@@ -57,145 +67,101 @@ ls /tmp/wind_turbine_download
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Reformat Data
+# MAGIC ## Create Raw Layer & Save
 # MAGIC Reformatting the data from mat to dataframes
 
 # COMMAND ----------
 
-# MAGIC %mkdir /tmp/souzan/
-# MAGIC %mkdir /tmp/souzan/turbine
+# MAGIC %md
+# MAGIC ### Process the data
 
 # COMMAND ----------
 
-# MAGIC %fs ls /mnt/quentin-demo-resources/turbine
-
-# COMMAND ----------
-
-display(spark.read.parquet('/mnt/quentin-demo-resources/turbine/gold/'))
-
-# COMMAND ----------
-
-# MAGIC %sql 
-# MAGIC select * from parquet.`/mnt/quentin-demo-resources/turbine/incoming-data`
-
-# COMMAND ----------
-
-# MAGIC %sh ls /tmp/wind_turbine_download/Damaged
-
-# COMMAND ----------
-
-# MAGIC %sh head /tmp/wind_turbine_download/Damaged/D1.mat
-
-# COMMAND ----------
-
-import os
-from scipy.io import loadmat
-mats = []
-root_dir = '/tmp/wind_turbine_download/Damaged'
-for root, dirs, files in os.walk(root_dir):
-    for file in files:
-        mats.append(loadmat(root_dir+'/'+file))
-
-# COMMAND ----------
-
-import numpy as np
-combined_mat = {}
-keys = ['Speed', 'Torque', 'AN3', 'AN4', 'AN5', 'AN6', 'AN7', 'AN8', 'AN9', 'AN10']
-for k in keys:
-  combined_mat[k] = np.concatenate([n[k] for n in mats]).flatten()
-  print(k, combined_mat[k][0])
-
-# COMMAND ----------
-
-# MAGIC %pip install faker
-
-# COMMAND ----------
-
-from faker import Faker
-fake = Faker()
-
-
-
-# COMMAND ----------
-
-help(fake.date_between)
-
-# COMMAND ----------
-
-k, combined_mat[k]
-
-# COMMAND ----------
-
-#check that there is enough entries for each sensor
-from collections import defaultdict 
-for k in keys:
-  print(k, combined_mat[k].size)
-
-#create the json data format
-wind_turbine_data = []
-for i in range(10): #combined_mat[k].size):
-  json_data = {}
-  for k in keys:
-    json_data[k] = combined_mat[k][i] 
-    json_data['TIMESTAMP'] = fake.date_time_between(start_date='now', end_date='-30d')
-  wind_turbine_data.append({'key': i, 'value': json_data}) 
-    
-
-# COMMAND ----------
-
-wind_turbine_data
-
-# COMMAND ----------
-
-from pyspark.sql.types import *
-
-# COMMAND ----------
-
-schema = StructType([StructField("Speed", StringType(), True),
-StructField("Torque", StringType(), True),
-StructField("AN3", StringType(), True),
-StructField("AN4", StringType(), True),
-StructField("AN5", StringType(), True),
-StructField("AN6", StringType(), True),
-StructField("AN7", StringType(), True),
-StructField("AN8", StringType(), True),
-StructField("AN9", StringType(), True),
-StructField("AN10", StringType(), True),
+schema = StructType([
+                    StructField('Speed', FloatType(), True),
+                    StructField('Torque', FloatType(), True),
+                    StructField('AN3', FloatType(), True),
+                    StructField('AN4', FloatType(), True),
+                    StructField('AN5', FloatType(), True),
+                    StructField('AN6', FloatType(), True),
+                    StructField('AN7', FloatType(), True),
+                    StructField('AN8', FloatType(), True),
+                    StructField('AN9', FloatType(), True),
+                    StructField('AN10', FloatType(), True),
+                    StructField('status', StringType(), True),
   ])
- 
+turbine_sensor_data = spark.createDataFrame([], schema)
+display(turbine_sensor_data)
 
 # COMMAND ----------
 
-type(combined_mat)
+damaged_root_dir = '/tmp/wind_turbine_download/Damaged'
+healthy_root_dir = '/tmp/wind_turbine_download/Healthy'
+keys = ['Speed', 'Torque', 'AN3', 'AN4', 'AN5', 'AN6', 'AN7', 'AN8', 'AN9', 'AN10']
+
+for root, dirs, files in os.walk(damaged_root_dir):
+    for file in files:
+      mat_file = {k: v.flatten() for k, v in loadmat(damaged_root_dir+'/'+file).items() if k in keys}
+      turbine_sensor_data = turbine_sensor_data.unionByName(pd.DataFrame(mat_file, columns=keys, dtype=float).to_spark().withColumn('status', lit('damaged')))
 
 # COMMAND ----------
 
-df = spark.createDataFrame(data=combined_mat, schema = schema)
+for root, dirs, files in os.walk(healthy_root_dir):
+    for file in files:
+      mat_file = {k: v.flatten() for k, v in loadmat(healthy_root_dir+'/'+file).items() if k in keys}
+      turbine_sensor_data = turbine_sensor_data.unionByName(pd.DataFrame(mat_file, columns=keys, dtype=float).to_spark().withColumn('status', lit('healthy')))
+
+# COMMAND ----------
+
+turbine_sensor_data = turbine_sensor_data.withColumn('id', when(col('status')=='healthy', (rand(seed=1)*500).cast('int')).otherwise((rand(seed=1)*(1501-500)+500).cast('int'))).withColumn('key', monotonically_increasing_id())
+
+# COMMAND ----------
+
+display(turbine_sensor_data)
+
+# COMMAND ----------
+
+fake = Faker()
+def fake_time():
+  return fake.date_time_between(start_date=datetime.date(2014,2,28), end_date=datetime.date(2014,3,28))
+fake_time_udf = udf(fake_time, TimestampType())
+
+turbine_sensor_data = turbine_sensor_data.withColumn('Timestamp', fake_time_udf())
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Move Data to storage
-# MAGIC 
-# MAGIC Move the Jigsaw train and test data from the driver node to object storage so that it can be be ingested into Delta Lake.
+# MAGIC ### Create Bronze Data & Move to Storage
 
 # COMMAND ----------
 
-for file in ['train','test','match','match_outcomes','player_ratings','players','chat','cluster_regions']:
-  print(f"moving data from file:/tmp/toxicity_download/{file}.csv to {cloud_storage_path}/{file}/bronze.csv")
-  dbutils.fs.mv(f"file:/tmp/toxicity_download/{file}.csv", f"{cloud_storage_path}/{file}/bronze.csv")
-  print(file)
+cloud_storage_path
 
 # COMMAND ----------
 
-# MAGIC %sh ls /dbfs/mnt/field-demos/manufacturing/iot_turbine/
+# DBTITLE 1,incoming_data_json
+ turbine_sensor_data.drop('index', 'status').write.mode('overwrite').json(f'{cloud_storage_path}/incoming-data-json')
+
+# COMMAND ----------
+
+# DBTITLE 1,incoming_data parquet
+turbine_sensor_data.withColumn('value', to_json(struct(col("Speed"), col("Torque"), col("AN3"), col("AN4"), col("AN5"), col("AN6"), col("AN7"), col("AN8"), col("AN9"), col("AN10"), col("id"), col("key"), col("Timestamp")))).select("key", "value").write.mode('overwrite').parquet(f'{cloud_storage_path}/incoming-data')
+
+# COMMAND ----------
+
+# DBTITLE 1,status
+turbine_sensor_data.select('id', 'status').write.mode('overwrite').parquet(f'{cloud_storage_path}/status')
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Copyright Databricks, Inc. [2021]. The source in this notebook is provided subject to the [Databricks License](https://databricks.com/db-license-source).  All included or referenced third party libraries are subject to the licenses set forth below.
+# MAGIC Copyright Databricks, Inc. [2022]. The source in this notebook is provided subject to the [Databricks License](https://databricks.com/db-license-source).  All included or referenced third party data are subject to the licenses set forth below.
 # MAGIC 
-# MAGIC |Library Name|Library license | Library License URL | Library Source URL |
+# MAGIC |Dataset Name|Dataset license | Dataset License URL | Dataset Source URL |
 # MAGIC |---|---|---|---|
-# MAGIC |Spark-nlp|Apache-2.0 License| https://github.com/JohnSnowLabs/spark-nlp/blob/master/LICENSE | https://github.com/JohnSnowLabs/spark-nlp/
-# MAGIC |Kaggle|Apache-2.0 License |https://github.com/Kaggle/kaggle-api/blob/master/LICENSE|https://github.com/Kaggle/kaggle-api|
+# MAGIC |Wind Turbine Gearbox Condition Monitoring Vibration Analysis Benchmarking Datasets|Creative Commons 4.0 License| https://data.openei.org/files/738/WindTurbineConditionMonitoringLicenseInfo.txt | https://data.openei.org/submissions/738
+# MAGIC <!-- |Kaggle|Apache-2.0 License |https://github.com/Kaggle/kaggle-api/blob/master/LICENSE|https://github.com/Kaggle/kaggle-api| -->
+
+# COMMAND ----------
+
+
